215cdf9ddd6b2029b29a6b7aad78cd13ddcb3c28
CristopherZuletaRestrepo <czuletar@unal.edu.co>
2026-07-05 10:48:01 -0500

    Add files via upload

--- ContactsManagement/src/contactsmanagement/Main.java
+++ ContactsManagement/src/contactsmanagement/Main.java
@@ -1,10 +1,12 @@
-
 package contactsmanagement;
 
+import gui.ContactsList;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+        ContactsList contactslist;
+        contactslist = new ContactsList();
+        contactslist.setVisible(true);
     }
 }
